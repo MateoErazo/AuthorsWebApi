a6cbcf91200439e3952bc106991dec3171cb9876
Jesús Mateo <erazojesusmateo@gmail.com>
2024-11-07 18:14:02 -0500

    I add tests for root controller using mock library

--- AuthorsWebApi.Tests/UnitTests/FirstCapitalLetterAttributeTests.cs
+++ AuthorsWebApi.Tests/UnitTests/FirstCapitalLetterAttributeTests.cs
@@ -21,5 +21,35 @@ namespace AuthorsWebApi.Tests.UnitTests
             //verification
             Assert.AreEqual("The first letter should be uppercase.", result.ErrorMessage);
         }
+
+        [TestMethod]
+        public void NullValue_DoesNotReturnError()
+        {
+            //preparation
+            var firstCapitalLetter = new FirstCapitalLetterAttribute();
+            string value = null;
+            var valContext = new ValidationContext(new { Name = value });
+
+            //execution
+            ValidationResult result = firstCapitalLetter.GetValidationResult(value, valContext);
+
+            //verification
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void FirstCapitalLetter_DoesNotReturnError()
+        {
+            //preparation
+            var firstCapitalLetter = new FirstCapitalLetterAttribute();
+            string value = "Jesus";
+            var valContext = new ValidationContext(new { Name = value });
+
+            //execution
+            ValidationResult result = firstCapitalLetter.GetValidationResult(value, valContext);
+
+            //verification
+            Assert.IsNull(result);
+        }
     }
 }
